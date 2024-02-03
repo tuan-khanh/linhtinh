c9aa5ad0b094f6fe12fe563e0cfdecaffe1e28ae
khanhnt <nguyentuankhanhcqt@gmail.com>
2024-02-04 00:18:24 +0700

    init pages

---
+++ auth/index.js
@@ -0,0 +1,15 @@
+const account = [
+  {
+    username: "admin",
+    password: "admin",
+  },
+];
+
+
+$(document).ready(function () {
+  
+function login () {
+    let username = $("#usernameInput").val()
+}
+
+});
